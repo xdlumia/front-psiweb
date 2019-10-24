cfbe3ffe3402ca0ace0cc25ecd338947a807dbf0
xdlumia <xdq@live.cn>
2019-10-24 18:08:58 +0800

    no message

--- public/IPconfig.js
+++ public/IPconfig.js
@@ -2,11 +2,11 @@
  * @Author: web.王晓冬
  * @Date: 2019-09-23 10:45:29
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-10-24 16:34:45
+ * @LastEditTime: 2019-10-24 17:49:04
  * @Description: ip配置文件
  */
 // let model = 'build' // dev 为开发环境 build 为测试环境  product 为生产环境
-let apis = '/apis/'
+let apisUrl = '/apis/'
 // apis = 'http://39.105.103.188:7011/'
 
 // if (model === 'dev') {
@@ -18,18 +18,14 @@ let apis = '/apis/'
 // }
 
 let baseURL = {
-  systemService: apis + 'system-service', // 角色，员工，部门
-  companyInitService: apis + 'company-init-service',
-  resourceService: apis + 'resource-service', // 9013
-  seeExternService: apis + 'see-extern-service', // 9024 see-extern-service
-  bizSystemService: apis + 'biz-system-service', // 角色 ，员工，部门8011
-  seeBaseHouseService: apis + 'see-base-house-service', // 楼盘配置
-  seeDataDictionaryService: apis + 'see-data-dictionary-service', // 字典服务
-  seeHouseConfigService: apis + 'see-baseinfo-service', // 在这个项目里 seeHouseConfigService 的公共部分都转移到 see-baseinfo-service服务里
-  seeBaseinfoService: apis + 'see-baseinfo-service', // 基础服务
-  // seePumaidongService: 'http://10.167.2.114:9539' // 铺脉动
-  seePayService: apis + 'see-pay-service',
-  cSystemService: apis + 'c-system-service'
+  systemService: apisUrl + 'system-service', // 角色，员工，部门
+  companyInitService: apisUrl + 'company-init-service',
+  resourceService: apisUrl + 'resource-service', // 9013
+//   bizSystemService: apisUrl + 'biz-system-service', // 角色 ，员工，部门8011
+  seeBaseHouseService: apisUrl + 'see-base-house-service', // 楼盘配置
+  seeDataDictionaryService: apisUrl + 'see-data-dictionary-service', // 字典服务
+  seeHouseConfigService: apisUrl + 'see-baseinfo-service', // 在这个项目里 seeHouseConfigService 的公共部分都转移到 see-baseinfo-service服务里
+  seeBaseinfoService: apisUrl + 'see-baseinfo-service', // 基础服务
 }
 window.g = {
   ApiUrl: baseURL,
