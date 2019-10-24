2cc6be373aa427d14361d5473e84670e721f58d9
zhaolun <zhaolun_git@fanteren.com>
2019-10-24 18:28:24 +0800

    eslint修正

--- src/utils/anchor.js
+++ src/utils/anchor.js
@@ -3,13 +3,13 @@ import Vue from 'vue'
 /**
  * @author 赵伦 2019-10-24
  * @description 在滚动区域内滚动到指定区域
- * 
+ *
  * @example 在例子中，需要声明ref变量，变量需要保证唯一性
- * 
+ *
  * <span v-anchor:target>点击后滚动到目标滚动区域</span>
- * 
- * <div ref="target">目标滚动区域</div> 
- * 
+ *
+ * <div ref="target">目标滚动区域</div>
+ *
  */
 
 Vue.directive('anchor', {
