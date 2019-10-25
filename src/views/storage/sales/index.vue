feb5dd7e2c3f5dc6925230d92515faac1baa01a7
xdlumia <xdq@live.cn>
2019-10-25 19:01:43 +0800

    修改代码定义

--- src/views/storage/sales/index.vue
+++ src/views/storage/sales/index.vue
@@ -9,47 +9,130 @@
   <div class="buying-requisition-page wfull hfull">
     <table-top>
     </table-top>
-     <d-table
+    <d-table
       api="seePumaidongService.collegeManagerList"
       :params="queryForm"
       ref="companyTable"
       class="college-main"
       style="height:calc(100% - 40px)"
     >
-      <el-table-column prop="title" label="销售出库单编号" min-width="140" show-overflow-tooltip>
+      <el-table-column
+        prop="title"
+        label="销售出库单编号"
+        min-width="140"
+        show-overflow-tooltip
+      >
         <template slot-scope="scope">
           <span class="d-text-blue">XSD20190201000001</span>
         </template>
       </el-table-column>
-      <el-table-column prop="cityName" min-width="100" label="客户名称" show-overflow-tooltip></el-table-column>
-      <el-table-column prop="title" label="销售单编号" min-width="140" show-overflow-tooltip>
+      <el-table-column
+        prop="cityName"
+        min-width="100"
+        label="客户名称"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        prop="title"
+        label="销售单编号"
+        min-width="140"
+        show-overflow-tooltip
+      >
         <template slot-scope="scope">
-          <span @click="getTableVisible(scope.row)" class="d-text-blue ac">XSD20190201000001</span>
+          <span
+            @click="getTableVisible(scope.row)"
+            class="d-text-blue ac"
+          >XSD20190201000001</span>
         </template>
       </el-table-column>
-      <el-table-column prop="cityName" min-width="100" label="出库状态" show-overflow-tooltip></el-table-column>
-      <el-table-column prop="cityName" min-width="100" label="发货状态" show-overflow-tooltip></el-table-column>
-      <el-table-column align="left" prop="cityName" label="组装任务状态" width="140" show-overflow-tooltip></el-table-column>
-      <el-table-column prop="cityName" min-width="100" label="拣货状态" show-overflow-tooltip></el-table-column>
-      <el-table-column prop="cityName" min-width="100" label="商品类别" show-overflow-tooltip></el-table-column>
-      <el-table-column prop="cityName" min-width="100" label="出库数量" show-overflow-tooltip></el-table-column>
-      <el-table-column prop="cityName" min-width="100" label="未出库量" show-overflow-tooltip></el-table-column>
-      <el-table-column prop="cityName" min-width="100" label="已出库量" show-overflow-tooltip></el-table-column>
-      <el-table-column prop="cityName" min-width="100" label="出库人" show-overflow-tooltip></el-table-column>
-      <el-table-column prop="createTime" label="生成时间" min-width="140" show-overflow-tooltip>
+      <el-table-column
+        prop="cityName"
+        min-width="100"
+        label="出库状态"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        prop="cityName"
+        min-width="100"
+        label="发货状态"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        align="left"
+        prop="cityName"
+        label="组装任务状态"
+        width="140"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        prop="cityName"
+        min-width="100"
+        label="拣货状态"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        prop="cityName"
+        min-width="100"
+        label="商品类别"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        prop="cityName"
+        min-width="100"
+        label="出库数量"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        prop="cityName"
+        min-width="100"
+        label="未出库量"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        prop="cityName"
+        min-width="100"
+        label="已出库量"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        prop="cityName"
+        min-width="100"
+        label="出库人"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        prop="createTime"
+        label="生成时间"
+        min-width="140"
+        show-overflow-tooltip
+      >
         <template slot-scope="scope">{{scope.row.createTime | timeToStr('YYYY-MM-DD HH:mm:ss')}}</template>
       </el-table-column>
-      <el-table-column prop="cityName" min-width="100" label="单据创建人" show-overflow-tooltip></el-table-column>
-      <el-table-column prop="cityName" min-width="100" label="创建部门" show-overflow-tooltip></el-table-column>
+      <el-table-column
+        prop="cityName"
+        min-width="100"
+        label="单据创建人"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        prop="cityName"
+        min-width="100"
+        label="创建部门"
+        show-overflow-tooltip
+      ></el-table-column>
     </d-table>
     <!-- 右侧滑出 -->
     <el-drawer
-      :title="visibleMeta.title"
+      :title="drawer.title"
       :visible.sync="tableVisible"
       direction="rtl"
-      size="50%">
+      size="50%"
+    >
       <div class='p10'>
-        <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tabs
+          v-model="activeName"
+          @tab-click="handleClick"
+        >
           <el-tab-pane label="详情"></el-tab-pane>
           <el-tab-pane label="拣货单"></el-tab-pane>
           <el-tab-pane label="组装任务"></el-tab-pane>
@@ -58,7 +141,10 @@
           <el-tab-pane label="借入单"></el-tab-pane>
           <el-tab-pane label="应收账单"></el-tab-pane>
         </el-tabs>
-        <components :is='visibleMeta.component' :visibleMeta="visibleMeta">
+        <components
+          :is='drawer.component'
+          :drawer="drawer"
+        >
         </components>
       </div>
     </el-drawer>
@@ -85,24 +171,24 @@ export default {
         page: 1,
         limit: 20
       },
-      componentActive:'',//当前的组件
-      tableVisible:false,//销售单右侧抽屉
-      visibleMeta:{//弹框的相关数据
-        title:'',
-        component:'Details'
+      componentActive: '',//当前的组件
+      tableVisible: false,//销售单右侧抽屉
+      drawer: {//弹框的相关数据
+        title: '',
+        component: 'Details'
       },
-      activeName:''
+      activeName: ''
     };
   },
   methods: {
     //点击打开右侧边栏
-    getTableVisible(data){
+    getTableVisible(data) {
       this.tableVisible = true
-      this.visibleMeta.title = '销售单'+data.id
+      this.drawer.title = '销售单' + data.id
     },
     //tab换组件
-    handleClick(){
-      
+    handleClick() {
+
     }
   }
 };
