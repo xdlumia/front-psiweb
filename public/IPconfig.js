33f9baeade0c788395f6b0af6bcaded4048fde04
xdlumia <xdq@live.cn>
2019-10-26 15:48:44 +0800

    添加样式

--- public/IPconfig.js
+++ public/IPconfig.js
@@ -2,7 +2,7 @@
  * @Author: web.王晓冬
  * @Date: 2019-09-23 10:45:29
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-10-25 12:03:41
+ * @LastEditTime: 2019-10-26 13:31:34
  * @Description: ip配置文件
  */
 // let model = 'build' // dev 为开发环境 build 为测试环境  product 为生产环境
@@ -19,14 +19,14 @@ apis = 'http://39.105.103.188:7011/'
 // }
 
 let baseURL = {
-//   systemService: apisUrl + 'system-service', // 角色，员工，部门
-//   companyInitService: apisUrl + 'company-init-service',
-//   resourceService: apisUrl + 'resource-service', // 9013
-// //   bizSystemService: apisUrl + 'biz-system-service', // 角色 ，员工，部门8011
-//   seeBaseHouseService: apisUrl + 'see-base-house-service', // 楼盘配置
-//   seeDataDictionaryService: apisUrl + 'see-data-dictionary-service', // 字典服务
-//   seeHouseConfigService: apisUrl + 'see-baseinfo-service', // 在这个项目里 seeHouseConfigService 的公共部分都转移到 see-baseinfo-service服务里
-//   seeBaseinfoService: apisUrl + 'see-baseinfo-service', // 基础服务
+  systemService: apisUrl + 'system-service', // 角色，员工，部门
+  //   companyInitService: apisUrl + 'company-init-service',
+  //   resourceService: apisUrl + 'resource-service', // 9013
+  // //   bizSystemService: apisUrl + 'biz-system-service', // 角色 ，员工，部门8011
+  //   seeBaseHouseService: apisUrl + 'see-base-house-service', // 楼盘配置
+  //   seeDataDictionaryService: apisUrl + 'see-data-dictionary-service', // 字典服务
+  //   seeHouseConfigService: apisUrl + 'see-baseinfo-service', // 在这个项目里 seeHouseConfigService 的公共部分都转移到 see-baseinfo-service服务里
+  //   seeBaseinfoService: apisUrl + 'see-baseinfo-service', // 基础服务
 }
 window.g = {
   ApiUrl: baseURL,
