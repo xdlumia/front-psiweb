868b05491fdece1333ad61abad522656964d0a07
zhaolun <zhaolun_git@fanteren.com>
2019-10-27 21:17:17 +0800

    採購單詳情

--- src/views/order/buying/detail.vue
+++ src/views/order/buying/detail.vue
@@ -2,20 +2,36 @@
  * @Author: 赵伦
  * @Date: 2019-10-26 10:12:11
  * @LastEditors: 赵伦
- * @LastEditTime: 2019-10-26 18:17:32
+ * @LastEditTime: 2019-10-26 19:05:19
  * @Description: 采购单详情
 */
 <template>
-    <side-popup :visible.sync="showPop">
-        <el-form>
-            <SideStatusbar />
-            <CustomerInfo />
-            <ApprovePanel />
-            <CustomInfo />
-            <CompanyInfo />
-            <PayExpire />
-            <ExtrasInfo />
-        </el-form>
+    <side-popup :visible.sync="showPop" class="side-page" title="请购单">
+        <el-container class="wfull hfull">
+            <el-header class="p0 d-bg-gray" style="height:70px;">
+                <div class="pl10 pr10 ar">
+                    <el-button size="mini" type="primary">采购</el-button>
+                    <el-button size="mini" type="primary">借入</el-button>
+                </div>
+                <SideStatusbar />
+            </el-header>
+            <el-main class="p0">
+                <el-tabs class="wfull hfull tabs-view">
+                    <el-tab-pane label="详情">
+                        <el-form>
+                            <CustomerInfo />
+                            <ApprovePanel />
+                            <CustomInfo />
+                            <CompanyInfo />
+                            <PayExpire />
+                            <ExtrasInfo />
+                        </el-form>
+                    </el-tab-pane>
+                    <el-tab-pane label="采购入库单">采购入库单</el-tab-pane>
+                    <el-tab-pane label="报价单">报价单</el-tab-pane>
+                </el-tabs>
+            </el-main>
+        </el-container>
     </side-popup>
 </template>
 <script>
@@ -45,4 +61,40 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.side-page {
+    /deep/ {
+        > .popup-main {
+            > .popup-head {
+                font-weight: bold;
+                > .d-inline > .popup-close {
+                    position: absolute;
+                    right: 10px;
+                    top: 16px;
+                }
+            }
+            > .popup-body {
+                padding: 0;
+                overflow: hidden;
+            }
+        }
+    }
+    .tabs-view {
+        position: relative;
+        /deep/ {
+            & > .el-tabs__header {
+                background-color: #f2f2f2;
+                padding: 0 10px;
+                > .el-tabs__nav-wrap::after {
+                    background-color: #f2f2f2;
+                }
+            }
+            & > .el-tabs__content {
+                height: calc(100% - 55px);
+                overflow: hidden;
+                overflow-y: auto;
+                padding: 0 10px;
+            }
+        }
+    }
+}
 </style>
