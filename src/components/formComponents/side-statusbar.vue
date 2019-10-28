fbd5525bb0b2681a9ba65e7c2f32c9d3c2f57a0a
xdlumia <xdq@live.cn>
2019-10-28 15:41:32 +0800

    改成flex布局

--- src/components/formComponents/side-statusbar.vue
+++ src/components/formComponents/side-statusbar.vue
@@ -16,11 +16,11 @@
 <template>
   <div style="margin-bottom:10px;">
     <div class="d-bg-gray side-statusbar" style="margin:-10px;padding:10px 20px;">
-      <el-row>
-        <el-col :key="item.label" :span="span" class="d-text-qgray" v-for="item of status">{{item.label}}</el-col>
+      <el-row type="flex">
+        <el-col :key="item.label" class="d-text-qgray d-elip" v-for="item of status">{{item.label}}</el-col>
       </el-row>
-      <el-row>
-        <el-col :key="item.label" :span="span" v-for="item of status">
+      <el-row type="flex">
+        <el-col :key="item.label" class="d-elip" v-for="item of status">
           <span v-if="item.isTime">{{item.value|timeToStr('YYYY-MM-DD hh:mm:ss')}}</span>
           <span v-else-if="item.dictName">{{item.value|dictionary(item.dictName)}}</span>
           <span v-else>{{item.value}}</span>
@@ -54,11 +54,6 @@ export default {
     return {};
   },
   computed: {
-    span() {
-      let length = this.status.length;
-      let baseSpan = Math.floor(24 / length);
-      return baseSpan;
-    }
   }
 };
 </script>
