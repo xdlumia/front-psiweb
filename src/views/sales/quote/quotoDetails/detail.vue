bc23706f9fa5f45771ec531597c3e986c004ee48
xdlumia <xdq@live.cn>
2019-10-31 10:54:35 +0800

    form表单全局引入

--- src/views/sales/quote/quotoDetails/detail.vue
+++ src/views/sales/quote/quotoDetails/detail.vue
@@ -2,7 +2,7 @@
  * @Author: web.王晓冬
  * @Date: 2019-10-24 12:33:49
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-10-29 19:31:25
+ * @LastEditTime: 2019-10-31 10:54:20
  * @Description: 报价单详情
 */
 <template>
@@ -16,7 +16,7 @@
     <!-- 发货信息 -->
     <deliverInfo />
     <!-- 商品信息 -->
-    <commoditInfo />
+    <commodityInfo />
     <!-- 报价有效期 -->
     <payExpire />
     <!-- 附加发票 -->
@@ -24,32 +24,13 @@
     <!-- 自定义信息 -->
     <customInfo />
     <!-- 备注信息 -->
-    <extraInfo />
+    <extrasInfo />
   </div>
 </template>
 <script>
-import approvePanel from '@/components/formComponents/approve-panel' //审核面板
-import customerInfo from '@/components/formComponents/customer-info' //客户信息
-import companyInfo from '@/components/formComponents/company-info' //公司信息
-import customInfo from '@/components/formComponents/custom-info' //自定义信息
-import deliverInfo from '@/components/formComponents/deliver-info' //发货信息
-import extrataxInfo from '@/components/formComponents/extratax-info' //附加发票
-import extraInfo from '@/components/formComponents/extras-info' //备注信息
-import commoditInfo from '@/components/formComponents/commodity-info' //商品信息
-import payExpire from '@/components/formComponents/pay-expire' //报价单有效期
-
 
 export default {
   components: {
-    approvePanel,
-    customerInfo,
-    companyInfo,
-    customInfo,
-    deliverInfo,
-    extrataxInfo,
-    extraInfo,
-    commoditInfo,
-    payExpire
   },
   props: ['drawerData'],
   data() {
