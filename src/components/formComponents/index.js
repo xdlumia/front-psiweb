efead21d554b0352a612bd5330dd77a277ad991b
xdlumia <xdq@live.cn>
2019-10-31 10:59:43 +0800

    全局组件引入加入说明

--- src/components/formComponents/index.js
+++ src/components/formComponents/index.js
@@ -2,8 +2,8 @@
  * @Author: web.王晓冬
  * @Date: 2019-10-30 10:27:10
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-10-31 10:46:36
- * @Description: file content
+ * @LastEditTime: 2019-10-31 10:59:19
+ * @Description: 全局引入form表单组件 自动将当前文件夹下的vue文件全局引入
  */
 
 const files = require.context('.', true, /\.vue$/)
