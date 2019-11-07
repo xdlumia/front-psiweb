fabdc003f4cd9fb3e441e973705cb69a1b3bdfc2
xdlumia <xdq@live.cn>
2019-11-07 17:09:35 +0800

    no message

--- src/components/formComponents/product-list.vue
+++ src/components/formComponents/product-list.vue
@@ -2,23 +2,22 @@
  * @Author: web.王晓冬
  * @Date: 2019-10-26 10:12:11
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-11-07 14:27:07
+ * @LastEditTime: 2019-11-07 17:07:51
  * @Description: 整机列表 和 配件列表   默认显示整机列表
 */
 <template>
   <form-card :title="title">
-    <!-- <d-table
+    <el-table
+      height="550px"
       v-if="title=='整机列表'"
-      refs="kind1"
-      api="seeGoodsService.selectGoodsNameList"
+      :data="kind1Data"
       :params="params"
       border
-      :paging="false"
       @selection-change="selectionChange"
-      :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+      :tree-props="{children: 'nameList', hasChildren: 'hasChildren'}"
     >
       <el-table-column
-        prop="id"
+        prop="className"
         label="商品分类"
         width="180"
       >
@@ -29,25 +28,27 @@
       >
       </el-table-column>
       <el-table-column
-        prop="id"
+        prop="name"
         label="商品名称"
-        width="180"
+        min-width="180"
       >
       </el-table-column>
-    </d-table> -->
-    <!-- <d-table
+
+    </el-table>
+    <el-table
+      height="550px"
+      :data="kind1Data"
       v-else
       ref="kind2"
       api="seeGoodsService.selectGoodsInfoTreeList"
-      :params="querForm"
+      :params="params"
       border
-      :paging="false"
       default-expand-all
       @selection-change="selectionChange"
       :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
     >
       <el-table-column
-        prop="id"
+        prop="firstClassName"
         label="商品分类"
         width="180"
       >
@@ -58,23 +59,48 @@
       >
       </el-table-column>
       <el-table-column
-        prop="id"
+        label="编号"
+        min-width="120"
+      >
+        <template slot-scope="scope">
+          {{scope.row.categoryCode}}
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="name"
         label="商品名称"
-        width="180"
+        min-width="120"
       >
       </el-table-column>
-    </d-table> -->
+      <el-table-column
+        prop="specOne"
+        min-width="120"
+        label="规格"
+      >
+      </el-table-column>
+    </el-table>
   </form-card>
 </template>
 <script>
 
 export default {
   props: {
-    title: '整机列表',
+    title: {
+      type: String,
+      default: '整机列表'
+    },
+    params: {
+      type: Object,
+      default: '整机列表'
+    },
   },
   data() {
     return {
       checked: [],
+      // 整机列表数据
+      kind1Data: [],
+
+      kind2Data: [],
       // 遍历表单
       querForm: {
         name: '', //商品名称
@@ -86,11 +112,26 @@ export default {
   methods: {
     reload() {
       if (this.title == '整机列表') {
-        this.$refs.kind1.reload(1)
+        this.selectGoodsNameList()
       } else {
-        this.$refs.kind1.reload(1)
+        this.selectGoodsInfoTreeList()
       }
     },
+    // 获取整机列表
+    selectGoodsNameList() {
+      this.$api.seeGoodsService.selectGoodsNameList(this.params)
+        .then(res => {
+          this.kind1Data = res.data || []
+        })
+    },
+    // 获取配件列表
+    selectGoodsInfoTreeList() {
+      this.$api.seeGoodsService.selectGoodsInfoTreeList(this.params)
+        .then(res => {
+          this.kind2Data = res.data || []
+        })
+    },
+    // 多选
     selectionChange(val) {
       this.$emit('selection-change', val)
     }
--- src/views/sales/quote/add/select-product.vue
+++ src/views/sales/quote/add/select-product.vue
@@ -2,7 +2,7 @@
  * @Author: web.王晓冬
  * @Date: 2019-10-24 12:33:49
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-11-07 14:21:56
+ * @LastEditTime: 2019-11-07 17:09:03
  * @Description: 选择产品
 */
 <template>
@@ -13,8 +13,9 @@
     >
       <!-- 选择类型 -->
       <el-select
-        v-model="productType"
+        v-model="queryForm.categoryCode"
         size="small"
+        @change="categoryChange"
         placeholder="请选择"
       >
         <!-- //过滤掉服务字典 此处不需要 -->
@@ -26,6 +27,17 @@
         >
         </el-option>
       </el-select>
+
+      <el-input
+        size="small"
+        placeholder="请输入名称"
+        v-model="queryForm.name"
+      >
+        <el-button
+          slot="append"
+          icon="el-icon-search"
+        >搜索</el-button>
+      </el-input>
       <!-- <el-select
         size="small"
         placeholder="请输入名称"
@@ -53,7 +65,7 @@
           width="400"
           trigger="click"
         >
-          <el-table :data="gridData">
+          <el-table :data="[...gridData,...gridData2]">
             <el-table-column
               width="50"
               property="date"
@@ -88,19 +100,18 @@
     <el-divider></el-divider>
     <product-list
       ref="kind1"
-      v-show="productType == 'PSI_SP_KIND-1'"
-      :params="{}"
+      v-show="queryForm.categoryCode == 'PSI_SP_KIND-1'"
+      :params="queryForm"
       title="整机列表"
       :data="KIND1Data"
-    ></product-list>
-    <!-- 
+    />
     <product-list
       ref="kind2"
-      :params="{}"
-      v-show="productType == 'PSI_SP_KIND-2'"
+      :params="queryForm"
+      v-show="queryForm.categoryCode == 'PSI_SP_KIND-2'"
       title="配件列表"
       :data="KIND2Data"
-    ></product-list> -->
+    />
   </div>
 </template>
 <script>
@@ -118,12 +129,31 @@ export default {
   data() {
     return {
       loading: false,
-      // 当前操作步骤
-      productType: 'PSI_SP_KIND-1', //默认选中整机列表
+      // 查询字段
+      queryForm: {
+        categoryCode: 'PSI_SP_KIND-1',//默认选中整机列表
+        name: ''
+      },
       KIND1Data: [], //整机列表
       KIND2Data: [], //配件列表
       // goodsOptions: [],
-
+      gridData2: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }],
       gridData: [{
         date: '2016-05-02',
         name: '王小虎',
@@ -144,40 +174,23 @@ export default {
     }
   },
   created() {
-  },
-  mounted() {
+
 
   },
+  mounted() {
+    // 初始化整机列表
+    this.$refs.kind1.reload()
+  },
   methods: {
-    // 获取列表
-    // async getClinent(words) {
-    //   if (words) {
-    //     let { data } = await this.$api.seePsiCommonService.commonclientinfoQueryList({
-    //       fuzzyNameOrCode: words,
-    //     });
-    //     this.goodsOptions = data || []
-    //   } else {
-    //     this.goodsOptions = [];
-    //   }
-    // },
-    // 获取客户详情信息
-    // commonclientinfoInfo() {
-    //   if (!this.data.clientId) {
-    //     this.$message.error({
-    //       showClose: true,
-    //       message: '客户内容不能为空'
-    //     })
-    //     return
-    //   }
-    //   this.loading = true
-    //   this.$api.seePsiCommonService.commonclientinfoInfo(null, this.data.clientId)
-    //     .then(res => {
-    //       this.clientInfo = res.data || {}
-    //     })
-    //     .finally(() => {
-    //       this.loading = false
-    //     })
-    // },
+    // 产品类型切换
+    categoryChange(code) {
+      if (code === 'PSI_SP_KIND-1') {
+        this.$refs.kind1.reload()
+      } else if (code === 'PSI_SP_KIND-2') {
+        this.$refs.kind2.reload()
+      }
+    },
+
     // 点击步骤条触发
     editClick() {
 
