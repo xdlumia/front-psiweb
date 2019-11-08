8d43dd453116fc7d760204979b427adf69c3c7da
xdlumia <xdq@live.cn>
2019-11-08 10:23:06 +0800

    公司信息组件字段对应

--- src/components/formComponents/company-info.vue
+++ src/components/formComponents/company-info.vue
@@ -2,7 +2,7 @@
  * @Author: 赵伦
  * @Date: 2019-10-26 10:12:11
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-11-07 14:30:25
+ * @LastEditTime: 2019-11-08 10:14:36
  * @Description: 公司信息
 */
 <template>
@@ -23,13 +23,13 @@
             v-if="item.type =='select'"
             :disabled='disabled'
             v-model="data[item.prop]"
-            :placeholder="`请输入${item.label}`"
+            :placeholder="`请选择${item.label}`"
           >
             <el-option
               v-for="item in item.options || []"
-              :key="item.code"
-              :label="item.content"
-              :value="item.code"
+              :key="item.id"
+              :label="item.corporationName"
+              :value="item.id"
             />
           </el-select>
           <el-input
@@ -47,8 +47,8 @@
 </template>
 <script>
 let formItems = [
-  { label: '公司发票账户', prop: 'companyAccountId', type: 'select', rules: [{ required: true, trigger: 'blur' }, { type: 'name' }], },
-  { label: '公司结算账户', prop: 'companySettlementId', type: 'select', rules: [{ required: true, trigger: 'blur' }], },
+  { label: '公司发票账户', prop: 'companyAccountId', type: 'select', rules: [{ required: true, trigger: 'blur' }], options: [] },
+  { label: '公司结算账户', prop: 'companySettlementId', type: 'select', rules: [{ required: true, trigger: 'blur' }], options: [] },
   { label: '发票抬头', prop: 'invoiceTitle', type: 'input', rules: [{ required: false, trigger: 'blur' }], },
   { label: '纳税人识别号', prop: 'taxpayersNum', type: 'input', rules: [{ required: false, trigger: 'blur' }, { type: 'phone' }], },
   { label: '注册地址', prop: 'registerAddres', type: 'input', rules: [{ required: false, trigger: 'blur' }], },
@@ -78,31 +78,35 @@ export default {
       formItems: formItems.filter(item => !this.hide.includes(item.prop)),
     }
   },
+  created() {
+    this.commoncorporationSelectForJxc()
+  },
   watch: {
     'data.companyAccountId': {
       handler(val) {
+        // 清空公司结算账户
+        this.data.companySettlementId = ''
         if (val) {
-          // 根据公司发票帐号id变化 获取公司阶段账户列表
-          this.getCompanySettlement(val)
+
+          // 获取公司发票下拉数据
+          let companyOption = this.formItems[0].options || []
+          // 获取公司发票选中项 
+          let [companySelected] = companyOption.filter(item => item.id === val)
+          // 给公司结算账户 options 添加下拉值
+          this.$set(this.formItems[1], 'options', companySelected.commonCorporationAccountEntities || [])
+          console.log(companySelected);
         }
       },
-      immediate: true,
       deep: true
     }
   },
   methods: {
-    // 获取公司发票账户
-    commonclientinfoInfo() {
-      this.$api.seePsiCommonService.commonclientinfoInfo(null, this.data.clientId)
+    // 获取公司发票账户 和结算账户 
+    commoncorporationSelectForJxc() {
+      this.$api.seeBaseinfoService.commoncorporationSelectForJxc()
         .then(res => {
-          this.formItems[0].options = res.data || []
-        })
-    },
-    // 公司结算账户
-    getCompanySettlement(id) {
-      this.$api.seePsiCommonService.commonclientinfoInfo(null, companyAccountId)
-        .then(res => {
-          this.formItems[1].options = res.data || []
+          // 给公司发票账户 options 添加下拉值
+          this.$set(this.formItems[0], 'options', res.data || [])
         })
     },
   },
