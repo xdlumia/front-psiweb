73906c5dc6ad7feb262012c505673e4dd9459549
高大鹏 <421524588@qq.com>
2019-11-11 19:22:10 +0800

    修改v-for视图不更新问题

--- src/views/basicSetting/quotation/components/config-info.vue
+++ src/views/basicSetting/quotation/components/config-info.vue
@@ -2,57 +2,59 @@
  * @Author: 高大鹏
  * @Date: 2019-11-09 12:27:18
  * @LastEditors: 高大鹏
- * @LastEditTime: 2019-11-11 18:39:04
+ * @LastEditTime: 2019-11-11 19:18:40
  * @Description: description
  -->
 <template>
   <div class v-loading="loading">
-    <form-card class="mb10" :title="true" v-if="!data.commonQuotationConfigDetailsEntitys.length">
-      <div slot="title" style="display:flex;align-items: center">
-        <span class="mr10">配置信息</span>
-        <el-button size="mini" type="primary" plain @click="add">+添加商品</el-button>
-      </div>
-    </form-card>
-    <form-card
-      class="mb10"
-      :title="index<1"
-      v-for="(itme, index) in data.commonQuotationConfigDetailsEntitys"
-      :key="index.commodityCode"
-    >
-      <div slot="title" style="display:flex;align-items: center">
-        <span class="mr10">配置信息</span>
-        <el-button size="mini" type="primary" plain @click="add">+添加商品</el-button>
-      </div>
-      <el-row :gutter="10" class="d-relative">
-        <el-col :span="8">
-          <el-form-item label="商品编号">
-            <el-input v-model="itme.commodityCode" disabled></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="8">
-          <el-form-item label="商品名称">
-            <el-input v-model="itme.goodsName" disabled></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="8">
-          <el-form-item label="所属分类">
-            <el-input v-model="itme.className" disabled></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="8">
-          <el-form-item label="商品数量">
-            <el-input v-model="itme.commodityNum"></el-input>
-          </el-form-item>
-        </el-col>
-        <el-button
-          round
-          style="padding:0;position:absolute;right:-4px;top:-9px;border:none"
-          @click="data.commonQuotationConfigDetailsEntitys.splice(index, 1)"
-        >
-          <i class="el-icon-remove" style="font-size:22px"></i>
-        </el-button>
-      </el-row>
-    </form-card>
+    <div>
+      <form-card class="mb10" :title="true" v-if="!data.commonQuotationConfigDetailsEntitys.length">
+        <div slot="title" style="display:flex;align-items: center">
+          <span class="mr10">配置信息</span>
+          <el-button size="mini" type="primary" plain @click="add">+添加商品</el-button>
+        </div>
+      </form-card>
+      <form-card
+        class="mb10"
+        :title="index<1"
+        v-for="(itme, index) in data.commonQuotationConfigDetailsEntitys"
+        :key="index.commodityCode"
+      >
+        <div slot="title" style="display:flex;align-items: center">
+          <span class="mr10">配置信息</span>
+          <el-button size="mini" type="primary" plain @click="add">+添加商品</el-button>
+        </div>
+        <el-row :gutter="10" class="d-relative">
+          <el-col :span="8">
+            <el-form-item label="商品编号">
+              <el-input v-model="itme.commodityCode" disabled></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="商品名称">
+              <el-input v-model="itme.goodsName" disabled></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="所属分类">
+              <el-input v-model="itme.className" disabled></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="商品数量">
+              <el-input v-model="itme.commodityNum"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-button
+            round
+            style="padding:0;position:absolute;right:-4px;top:-9px;border:none"
+            @click="data.commonQuotationConfigDetailsEntitys.splice(index, 1)"
+          >
+            <i class="el-icon-remove" style="font-size:22px"></i>
+          </el-button>
+        </el-row>
+      </form-card>
+    </div>
     <commodity-choose
       :kinds="[{label:'配件',value:'PSI_SP_KIND-2'},{label:'服务',value:'PSI_SP_KIND-3'}]"
       :visible.sync="showCommodityGoods"
