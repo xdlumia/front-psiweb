7e9436f7b69337ef2649b083560e947b83d5e9b4
xdlumia <xdq@live.cn>
2019-11-15 15:56:57 +0800

    no message

--- src/utils/visibleMixin.js
+++ src/utils/visibleMixin.js
@@ -2,7 +2,7 @@
  * @Author: 赵伦
  * @Date: 2019-11-07 09:47:39
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-11-15 15:54:49
+ * @LastEditTime: 2019-11-15 15:56:52
  * @Description: 编辑、详情 visible 辅助 mixin ，这是一个和业务紧密结合的mixin，所以需要在特定业务环境下使用
  */
 
@@ -88,7 +88,7 @@ export default {
             if (data) {
               data = JSON.parse(JSON.stringify(data))
               this.detail = data || {}
-              this.rowData = data || {}
+              // this.rowData = data || {}
               if (this.form && this.type == 'edit') {
                 for (let key in this.form) {
                   if (this.form[key] instanceof Array) {
