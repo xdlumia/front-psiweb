c472625666c2384b33f7348ff13a5c44d25da739
xdlumia <xdq@live.cn>
2019-11-20 10:04:17 +0800

    no message

--- src/views/finance/income/index.vue
+++ src/views/finance/income/index.vue
@@ -2,20 +2,20 @@
  * @Author: web.王晓冬
  * @Date: 2019-08-23 14:12:30
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-11-18 09:04:06
- * @Description: 销售-销售退货单
+ * @LastEditTime: 2019-11-19 19:24:42
+ * @Description: 销售-收入流水
  */
 <template>
   <div>
     <table-view
-      busType="17"
+      busType="54"
       ref="table"
       :filter="true"
       :moreButton="true"
       :column="true"
-      title="销售退货单"
-      api="seePsiSaleService.salesreturnedList"
-      exportApi="seePsiSaleService.salesreturnedExport"
+      title="收入流水"
+      api="seePsiFinanceService.revenuerecordList"
+      exportApi="seePsiSaleService.revenuerecordExport"
       :params="Object.assign(queryForm,params)"
       :filterOptions="filterOptions"
     >
@@ -25,7 +25,7 @@
         <span
           class="d-text-blue d-pointer"
           v-if="column.columnFields=='alterationCode'"
-          @click="eventHandle('returnVisible',row)"
+          @click="eventHandle('detailVisible',row)"
         > {{value}}</span>
         <!-- 销售出库单编号 -->
         <span
@@ -41,25 +41,17 @@
       </template>
     </table-view>
     <!-- 销售退货单详情 -->
-    <returnDetails
-      v-if="returnVisible"
-      :visible.sync="returnVisible"
+    <detail
+      v-if="detailVisible"
+      :visible.sync="detailVisible"
       :rowData="rowData"
       :code="rowData.alterationCode"
-      @reload="this.$refs.table.reload()"
-    />
-    <!-- 销售出库单详情 -->
-    <outLibDetails
-      v-if="outLibVisible"
-      :visible.sync="outLibVisible"
-      :rowData="rowData"
-      :code="rowData.salesShipmentCode"
       @reload="this.$refs.table.reload()"
     />
   </div>
 </template>
 <script>
-import returnDetails from './details' //销售退货单详情
+import detail from './details' //销售退货单详情
 let filterOptions = [
   // { label: '商户编号、商户名称/简称', prop: 'alterationCode', default: true, type: 'text' },
   { label: '联系人、联系人电话', prop: 'shipmentCode', default: true, type: 'text' },
@@ -71,7 +63,7 @@ let filterOptions = [
 export default {
   name: 'return',
   components: {
-    returnDetails,
+    detail
 
   },
   props: {
@@ -111,7 +103,7 @@ export default {
       filterOptions: filterOptions,
       // 当前行数据
       rowData: {},
-      returnVisible: false,
+      detailVisible: false,
       outLibVisible: false,
     };
   },
