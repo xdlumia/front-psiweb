41774b5e2771c235fddd9f9192228863dc89221f
xdlumia <xdq@live.cn>
2019-12-03 12:38:57 +0800

    no message

--- src/views/sales/exchange/add.vue
+++ src/views/sales/exchange/add.vue
@@ -2,7 +2,7 @@
  * @Author: web.王晓冬
  * @Date: 2019-10-24 12:33:49
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-12-02 10:56:57
+ * @LastEditTime: 2019-12-03 11:43:52
  * @Description: 生成销售换货单
 */
 <template>
@@ -209,7 +209,7 @@ export default {
       this.$refs.form.validate(valid => {
         if (!this.form.shipmentFinanceSaveVoList.length) {
           this.$message({
-            message: '请添加账期信息',
+            message: '请添加其他费用',
             type: 'error',
             showClose: true,
           });
