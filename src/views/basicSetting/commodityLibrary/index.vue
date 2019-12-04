0bee5c9ef22d75741cbaca9bd783ba054a41fd07
高大鹏 <421524588@qq.com>
2019-12-04 21:41:23 +0800

    修改商品库筛选

--- src/views/basicSetting/commodityLibrary/index.vue
+++ src/views/basicSetting/commodityLibrary/index.vue
@@ -2,7 +2,7 @@
  * @Author: 高大鹏
  * @Date: 2019-10-30 14:43:46
  * @LastEditors: 高大鹏
- * @LastEditTime: 2019-12-04 15:39:16
+ * @LastEditTime: 2019-12-04 21:40:56
  * @Description: 商品管理
  -->
 <template>
@@ -37,7 +37,7 @@
               style="margin-top:2px;"
               title="关闭"
             ></el-link>
-            <dFilter v-model="goodsForm" :options="filterOptions" @change="getGoodsList" />
+            <dFilter v-model="goodsForm" :options="filterOptions" @change="goodsForm.page=1,getGoodsList()" />
             <el-button
               size="mini"
               class="tool-item"
@@ -89,7 +89,8 @@
                 >复制新增</el-button>
               </template>
             </el-table-column>
-            <el-table-column align="center" label="商品编号" min-width="160"
+            <el-table-column
+align="center" label="商品编号" min-width="160"
 show-overflow-tooltip>
               <template slot-scope="scope">
                 <span
@@ -194,7 +195,8 @@ show-overflow-tooltip>
       </div>
     </div>
 
-    <el-dialog :visible.sync="visible" title v-dialogDrag
+    <el-dialog
+:visible.sync="visible" title v-dialogDrag
 :show-close="false" width="1000px">
       <div slot="title" style="display:flex;">
         <h3 style="flex:1;text-align:center;">新增商品</h3>
@@ -203,7 +205,8 @@ show-overflow-tooltip>
           <el-button size="mini" @click="visible=false">关闭</el-button>
         </div>
       </div>
-      <add-good @refresh="refresh" v-if="visible" ref="addGood"
+      <add-good
+@refresh="refresh" v-if="visible" ref="addGood"
 :code="copycode" :copy="copy"></add-good>
     </el-dialog>
     <detail
