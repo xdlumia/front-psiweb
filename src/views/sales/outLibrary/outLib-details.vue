2912485ec5735f42007a4b9e59305dd56f24afbf
xdlumia <xdq@live.cn>
2019-12-09 14:46:14 +0800

    修改出库单详情detail

--- src/views/sales/outLibrary/outLib-details.vue
+++ src/views/sales/outLibrary/outLib-details.vue
@@ -2,7 +2,7 @@
  * @Author: web.王晓冬
  * @Date: 2019-10-24 12:33:49
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-12-07 19:18:27
+ * @LastEditTime: 2019-12-09 14:45:47
  * @Description: 销售出库单详情
 */
 <template>
@@ -56,7 +56,7 @@
           ref="detail"
           :params="activeName == 'contractSale'? {shipmentCode:code}:{relationCode:code}"
           :code="this.code"
-          :rowData="rowData"
+          :rowData="detail"
           :data="detail || {}"
           class="d-auto-y"
           :button="false"
