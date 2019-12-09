4ebf3bb8fd318467c580a3103588b0711d1a41d9
xdlumia <xdq@live.cn>
2019-12-09 18:33:43 +0800

    提交审核

--- src/views/finance/receipt/details.vue
+++ src/views/finance/receipt/details.vue
@@ -2,7 +2,7 @@
  * @Author: web.王晓冬
  * @Date: 2019-10-24 12:33:49
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-12-09 18:06:51
+ * @LastEditTime: 2019-12-09 18:32:58
  * @Description: 财务-收入流水详情
 <template>
   <div>
