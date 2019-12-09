b18fd49fa9c9348fbae8b4aaf359db462701c36f
xdlumia <xdq@live.cn>
2019-12-09 10:55:55 +0800

    no message

--- src/components/filter/date-range.vue
+++ src/components/filter/date-range.vue
@@ -1,12 +1,15 @@
 /*
  * @Author: 赵伦
  * @Date: 2019-11-01 10:46:22
- * @LastEditors: 赵伦
- * @LastEditTime: 2019-12-04 14:44:02
+ * @LastEditors: web.王晓冬
+ * @LastEditTime: 2019-12-09 10:55:46
  * @Description: 时间段过滤框
 */
 <template>
-  <el-form-item :label="item.label" :prop="`min${item.prop}`">
+  <el-form-item
+    :label="item.label"
+    :prop="`min${item.prop}`"
+  >
     <el-date-picker
       @change="update"
       class="wfull"
@@ -43,20 +46,20 @@ export default {
       let range = this.value || [];
       let minDate = new Date(+range[0] || '')
       let maxDate = new Date(+range[1] || '')
-      this.$set(this.form, `min${this.item.prop}`, +new Date(`${minDate.toLocaleDateString()} 00:00:00`));
-      this.$set(this.form, `max${this.item.prop}`, +new Date(`${maxDate.toLocaleDateString()} 23:59:59`));
+      this.$set(this.form, `min${firstToUpperCase(this.item.prop)}`, +new Date(`${minDate.toLocaleDateString()} 00:00:00`));
+      this.$set(this.form, `max${firstToUpperCase(this.item.prop)}`, +new Date(`${maxDate.toLocaleDateString()} 23:59:59`));
       this.changed()
     },
     reset() {
-      let min = this.form[`min${this.item.prop}`];
-      let max = this.form[`max${this.item.prop}`];
+      let min = this.form[`min${firstToUpperCase(this.item.prop)}`];
+      let max = this.form[`max${firstToUpperCase(this.item.prop)}`];
       if (min && max) {
         min = min ? new Date(min) : '';
         max = max ? new Date(max) : '';
         this.value = [min, max];
       } else {
-        this.form[`min${this.item.prop}`] = '';
-        this.form[`max${this.item.prop}`] = '';
+        this.form[`min${firstToUpperCase(this.item.prop)}`] = '';
+        this.form[`max${firstToUpperCase(this.item.prop)}`] = '';
         this.value = null;
       }
     },
