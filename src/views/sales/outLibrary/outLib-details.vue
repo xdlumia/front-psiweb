b07d9c6adf7aca51a30153868f9a3f83c9b24c7f
xdlumia <xdq@live.cn>
2019-12-11 16:24:14 +0800

    修改金额

--- src/views/sales/outLibrary/outLib-details.vue
+++ src/views/sales/outLibrary/outLib-details.vue
@@ -2,7 +2,7 @@
  * @Author: web.王晓冬
  * @Date: 2019-10-24 12:33:49
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-12-11 15:26:05
+ * @LastEditTime: 2019-12-11 16:23:52
  * @Description: 销售出库单详情
 */
 <template>
@@ -291,7 +291,7 @@ export default {
             busType: 0,
             purchaseId: this.detail.clientId,
             purchaseType: 0,
-            marketId: this.detail.companySettlementId,
+            marketId: this.detail.companyId,
             marketType: 3,
             invoiceDetailList: []
               .concat(
