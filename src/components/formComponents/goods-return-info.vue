5e82c9dfdb6fd49103db4b43177320df04b250b5
xdlumia <xdq@live.cn>
2019-12-14 12:49:32 +0800

    no message

--- src/components/formComponents/goods-return-info.vue
+++ src/components/formComponents/goods-return-info.vue
@@ -2,7 +2,7 @@
  * @Author: web.王晓冬
  * @Date: 2019-10-28 15:44:58
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-12-14 12:27:57
+ * @LastEditTime: 2019-12-14 12:48:54
  * @Description: 退货商品商品信息
 */
 <template>
@@ -76,7 +76,7 @@
             <span
               @click="changeRecord(scope)"
               class="d-text-blue d-pointer"
-            >{{scope.row.alterationNumber || 0}}/{{scope.row.commodityNumber}}</span>
+            >{{scope.row.refundNumber || 0}}/{{scope.row.commodityNumber}}</span>
           </template>
         </el-table-column>
         <el-table-column
