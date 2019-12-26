1e2310b47fe2fbff331c87f9c7b67da5494c216d
zhaolun <zhaolun_git@fanteren.com>
2019-12-26 16:42:00 +0800

    报价单

--- src/components/formComponents/commodity-quote-edit.vue
+++ src/components/formComponents/commodity-quote-edit.vue
@@ -2,7 +2,7 @@
  * @Author: 王晓冬
  * @Date: 2019-10-28 17:05:01
  * @LastEditors: 赵伦
- * @LastEditTime: 2019-12-26 15:14:51
+ * @LastEditTime: 2019-12-26 16:40:53
  * @Description: 新增销售报价单 商品信息 可编辑
 */  
 <template>
@@ -301,7 +301,7 @@
         prop="recentDiscountSprice"
       >
         <template slot-scope="{row}">
-          <span>{{row.recentDiscountSprice}}</span>
+          <span>{{row.recentDiscountSprice||0}}</span>
         </template>
       </el-table-column>
 
@@ -399,7 +399,9 @@ export default {
         child.commodityNumber = child.commodityNum
         child.reference = child.saleReferencePrice
         child.discountSprice = '-'
-        child.recentDiscountSprice = businessInfo[child.commodityCode].recentDiscountSprice
+        if(businessInfo[child.commodityCode]){
+          child.recentDiscountSprice = businessInfo[child.commodityCode].recentDiscountSprice
+        }
       })
       cb(data);
     },
