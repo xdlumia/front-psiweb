ea432418040673f5b53bdd7a3a61cd36631f951c
xdlumia <xdq@live.cn>
2020-01-03 17:54:44 +0800

    新增邮件担保

--- src/views/sales/outLibrary/outLib-details.vue
+++ src/views/sales/outLibrary/outLib-details.vue
@@ -2,7 +2,7 @@
  * @Author: web.王晓冬
  * @Date: 2019-10-24 12:33:49
  * @LastEditors: web.王晓冬
- * @LastEditTime: 2019-12-18 15:06:43
+ * @LastEditTime: 2020-01-03 17:51:39
  * @Description: 销售出库单详情
 */
 <template>
@@ -127,6 +127,14 @@
       @reload="setEdit(),$reload()"
     >
     </approvalTime>
+    <!-- 采购审核时间 -->
+    <emailAssure
+      v-if="emailVisible"
+      :visible.sync="emailVisible"
+      :rowData="detail"
+      @reload="setEdit(),$reload()"
+    >
+    </emailAssure>
   </div>
 </template>
 <script>
@@ -137,6 +145,7 @@ import returnAdd from '../return/add' //退货单新增
 import exchangeAdd from '../exchange/add' //换货单新增
 import collectInvoice from '@/views/finance/receipt/collect-invoice' //开票申请
 import approvalTime from './approvalTime' //审核采购时间
+import emailAssure from './email-assure' //审核采购时间
 
 import detail from './outLibDetails/detail' //详情
 import VisibleMixin from '@/utils/visibleMixin';
@@ -149,7 +158,8 @@ export default {
     returnAdd,
     exchangeAdd,
     collectInvoice,
-    approvalTime
+    approvalTime,
+    emailAssure
   },
 
   data() {
@@ -172,6 +182,8 @@ export default {
         { label: '合同完善', type: 'primary', authCode: 'psi_sales_outlibrary_10' },
         { label: '审核采购时间', type: 'primary', authCode: 'psi_sales_outlibrary_12' },
         { label: '合同收回', type: 'primary', authCode: 'psi_sales_outlibrary_13' },
+        // TODO 邮件担保权限码没有加
+        { label: '邮件担保', type: 'primary', authCode: 'psi_sales_outlibrary_13' },
         { label: '追加合同附件', type: 'primary', authCode: 'psi_sales_outlibrary_11' },
       ],
       stateText: {
@@ -191,7 +203,7 @@ export default {
         '-1': ['提交审核', '编辑', '删除', '生成合同'], // 新建
         '0': ['撤销审核', '审核通过', '驳回', '合同完善', '追加合同附件'], //审核中
         '1': ['审核采购时间', '追加合同附件'], //请购处理
-        '2': ['合同收回', '追加合同附件'], //合同收回
+        '2': ['合同收回', '邮件担保', '追加合同附件'], //合同收回
         '3': ['终止', '追加合同附件'], //已通过
         '4': ['生成退货单', '生成换货单', '开票申请', '追加合同附件'], //已完成
         '5': ['提交审核', '编辑', '删除', '编辑合同'], //已驳回
@@ -221,6 +233,7 @@ export default {
       exchangeAddVisible: false,
       collectInvoiceVisible: false, //开票申请
       timeApprovalVisible: false, //审核采购时间
+      emailVisible: false, //邮件担保
       collectInvoiceData: {},
 
     }
@@ -273,7 +286,8 @@ export default {
         '生成退货单': 'returnAddVisible',
         '生成换货单': 'exchangeAddVisible',
         '开票申请': 'collectInvoiceVisible',
-        '审核采购时间': 'timeApprovalVisible'
+        '审核采购时间': 'timeApprovalVisible',
+        '邮件担保': 'emailVisible'
       }
       // 需要弹出操作的功能
       if (labelObj.hasOwnProperty(label)) {
